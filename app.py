0a1876bed3572eaeab730f27d4831255a32a5aa8
dimad <dima.dmitriev17.02@gmail.com>
2022-05-21 21:54:57 +0300

    Создал репазиторий

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    database="service_db",
+    user="postgres",
+    password="12345",
+    host="localhost",
+    port="5432")
+
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html', error_code=0)
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if len(username) < 1 or len(password) < 1:
+        return render_template('login.html', error_code=1)
+    else:
+        cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+        records = list(cursor.fetchall())
+        if len(records) > 0:
+            return render_template('account.html', full_name=records[0][1], username=username, password=password)
+        else:
+            return render_template('login.html', error_code=2)
